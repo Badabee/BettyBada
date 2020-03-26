91294f56c64d6ea6cc04ed6343bb6f7de896761f
Badabee <badabee440@gmail.com>
2020-03-26 14:51:17 +0100

    updated js

---
+++ task3.js
@@ -0,0 +1,11 @@
+let name = "Bada";
+let courses= ["HTML", "CSS", "JavaScript"];
+console.log(name);
+console.log(courses);
+
+
+// for even numbers
+for (i = 0; i <=200; i++){
+    if (( i % 2) === 0)
+        console.log(i);
+}
